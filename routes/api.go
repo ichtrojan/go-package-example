e6eda2545d929b5e8a21a2b4fa45a99d19edae7c
ichtrojan <ordrizzy@gmail.com>
2019-11-04 12:36:37 +0100

    Ignore implicitly

--- routes/api.go
+++ routes/api.go
@@ -1,17 +1,17 @@
 package routes
 
 import (
-  "fmt"
-  "github.com/gorilla/mux"
-  "net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func Init() {
-  route := mux.NewRouter()
+	route := mux.NewRouter()
 
-  route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Hello World")
 	})
 
-  http.ListenAndServe(":9990", route)
+	_ = http.ListenAndServe(":9990", route)
 }
